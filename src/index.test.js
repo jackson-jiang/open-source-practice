e9044475b36d7cbf3d2ec948cbd58a3cd26e9b58
Jackson.Jiang <jackson.jiang@outlook.com>
2018-07-04 20:27:41 +0800

    feat(ss): fff
    ff

--- src/index.test.js
+++ src/index.test.js
@@ -3,7 +3,7 @@ var expect =require('chai').expect;
 
 describe('champion-names', function() {
   it('共5个国家', function() {
-    expect(champion.all().length).to.equal(4);
+    expect(champion.all().length).to.equal(5);
   });
   it('should contain `巴西`', function() {
     expect(champion.all()).to.include('巴西');
