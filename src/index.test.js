08615f5702fd4b3d21102dc60cb3b4a775f8c72d
Jackson.Jiang <jackson.jiang@outlook.com>
2018-07-04 20:21:19 +0800

    feat(ss): ssss
    ffff

---
+++ src/index.test.js
@@ -0,0 +1,14 @@
+var champion = require('./index.js')
+var expect =require('chai').expect;
+
+describe('champion-names', function() {
+  it('共5个国家', function() {
+    expect(champion.all().length).to.equal(4);
+  });
+  it('should contain `巴西`', function() {
+    expect(champion.all()).to.include('巴西');
+  });
+  it('should contain `巴西`', function() {
+    expect(champion.all()).to.include(champion.champion());
+  });
+});
